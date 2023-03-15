6787582412b94a2a2853ca024e0e9248bd4e8e5f
Amith George <amith.george@lifecheq.co.za>
2023-03-15 07:18:34 +0000

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to zprint-autocommit-demo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lifecheq/zprint_autocommit_demo_test.clj
@@ -0,0 +1,7 @@
+(ns lifecheq.zprint-autocommit-demo-test
+  (:require [clojure.test :refer :all]
+            [lifecheq.zprint-autocommit-demo :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
